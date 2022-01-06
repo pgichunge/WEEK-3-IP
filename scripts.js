ed06c8cda6d469b4ffbe451eb137fee895d4283d
pamela mwendwa gichunge <pgichunge@safaricom.co.ke>
2022-01-07 00:16:24 +0300

    make changes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
 jQuery(document).ready(function() {
-    jQuery("h1").click(function() {
-      alert("This is a header.");
-    });
-  
-    jQuery("p").click(function() {
-      alert("This is a paragraph.");
-    });
-  
-    jQuery("img").click(function() {
-      alert("WELCOME TO DELANI STUDIO.AMAZING PEOPLE ARE MAKING AMAZING DESIGNS IN A FUN ENVIRONMENT");
+    
+jQuery("h2").click(function() {
+      alert("PRODUCT MANAGEMENT.Planning and development is iterative. Because we are constantly coding and testing, the products we build are always ready to go live. This iterative process allows for changes as business requirements evolve.");
     });
   });
+  jQuery(document).ready(function() {
+    
+    jQuery("h3").click(function() {
+          alert("DESIGN Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing. Throughout your project, our designers create and implement visual design and workflows, solicit user feedback and work with you to make sure what gets built is what is needed..");
+        });
+      });
