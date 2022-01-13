54390adecd0dce6cbe25f6da1fda45966fd95e13
pamela mwendwa gichunge <pgichunge@safaricom.co.ke>
2022-01-13 15:52:41 +0300

    make changes

--- scripts.js
+++ scripts.js
@@ -9,6 +9,13 @@ jQuery(document).ready(function() {
       jQuery("h3").click(function() {
             alert("DESIGN Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing. Throughout your project, our designers create and implement visual design and workflows, solicit user feedback and work with you to make sure what gets built is what is needed..");
           });
-        });     
+        }); 
+        jQuery(document).ready(function() {
+      
+          jQuery("h3").click(function() {
+                alert("DevelopmentAll engineers are fluent in the latest enterprise, mobile and web development technologies. They collaborate with your team to write, and improve code on a daily basis, using proven practices such as test-driven development and pair programming.");
+              });
+            }); 
+
 
 
