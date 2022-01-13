8cb86f0c4b9640d6d9f207e73f260ede0060ff36
pamela mwendwa gichunge <pgichunge@safaricom.co.ke>
2022-01-14 00:16:53 +0300

    add changes

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 jQuery(document).ready(function() {
     
-  jQuery("h2").click(function() {
+  jQuery("#prd1").click(function() {
         alert("PRODUCT MANAGEMENT.Planning and development is iterative. Because we are constantly coding and testing, the products we build are always ready to go live. This iterative process allows for changes as business requirements evolve.");
       });
     });
     jQuery(document).ready(function() {
       
-      jQuery("h3").click(function() {
+      jQuery("#prd2").click(function() {
             alert("DESIGN Our design practice offers a full range of services including brand strategy, interaction and visual design and user experience testing. Throughout your project, our designers create and implement visual design and workflows, solicit user feedback and work with you to make sure what gets built is what is needed..");
           });
         }); 
         jQuery(document).ready(function() {
       
-          jQuery("h3").click(function() {
+          jQuery("#prd3").click(function() {
                 alert("DevelopmentAll engineers are fluent in the latest enterprise, mobile and web development technologies. They collaborate with your team to write, and improve code on a daily basis, using proven practices such as test-driven development and pair programming.");
               });
             }); 
